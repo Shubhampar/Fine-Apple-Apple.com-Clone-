76fe58122a0012697387388b47465e6f84ba3135
shivamvr <shivamweb2@gmail.com>
2023-03-30 21:34:43 +0530

    minor changes

--- index.js
+++ index.js
@@ -31,9 +31,20 @@ let navItems = document.querySelectorAll('.item')
 
 function expandOptions(e){
  e.querySelector('div').style.display = 'flex'
- console.log( e.querySelector('div'))
+//  console.log( e.querySelector('div'))
 }   
 function closeOptions(e){
  e.querySelector('div').style.display = 'none'
  console.log( e.querySelector('div'))
-}   
+}  
+
+function goto(page){
+  console.log(page)
+  switch(page){
+  }
+
+  if(page == 'mac'){
+    window.location.href = '/mac.html'
+  }
+
+}
